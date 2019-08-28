72eaec0ac72c2ce338e47f733102c0c8b7948cdf
Maximilian Stümpfl <maximilian2304@t-online.de>
2019-08-28 23:31:53 +0200

    Added simple movement
    and animation, and more sprite for the animations

--- Assets/_Scripts/Player/PlayerController.cs
+++ Assets/_Scripts/Player/PlayerController.cs
@@ -1,77 +1,98 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class PlayerController : MonoBehaviour
 {
 
-    private Rigidbody2D rigidbody2D;
-    private SpriteRenderer spriteRenderer;
+    [SerializeField] private float jumpForce = 400f;                          // Amount of force added when the player jumps.
+    [Range(0, 1)] [SerializeField] private float crouchSpeed = .36f;          // Amount of maxSpeed applied to crouching movement. 1 = 100%
+    [SerializeField] private float walkSpeed = 12;          // Amount of maxSpeed applied to crouching movement. 1 = 100%
+    [Range(0, .3f)] [SerializeField] private float m_MovementSmoothing = .05f;  // How much to smooth out the movement
+    [SerializeField] private bool m_AirControl = false;                         // Whether or not a player can steer while jumping;
+    [SerializeField] private LayerMask m_WhatIsGround;                          // A mask determining what is ground to the character
+    [SerializeField] private Transform m_GroundCheck;                   // A position marking where to check if the player is grounded.
+    [SerializeField] private Transform m_CeilingCheck;                          // A position marking where to check for ceilings
+    [SerializeField] private Collider2D m_CrouchDisableCollider;                // A collider that will be disabled when crouching
 
+    const float k_GroundedRadius = .2f; // Radius of the overlap circle to determine if grounded
+    private bool m_Grounded;            // Whether or not the player is grounded.
+    const float k_CeilingRadius = .2f; // Radius of the overlap circle to determine if the player can stand up
+    private Rigidbody2D m_Rigidbody2D;
+    private bool m_FacingRight = true;  // For determining which way the player is currently facing.
+    private Vector3 m_Velocity = Vector3.zero;
 
-    [SerializeField]
-    private float speed = 3f;
-    [SerializeField]
-    private float jumpVelocity = 4f;
+    [Header("Events")]
+    [Space]
 
+    public UnityEvent OnLandEvent;
+
+    [System.Serializable]
+    public class BoolEvent : UnityEvent<bool> { }
+
+    public BoolEvent OnCrouchEvent;
+    private bool m_wasCrouching = false;
 
     private void Awake()
     {
-        
+        m_Rigidbody2D = GetComponent<Rigidbody2D>();
+
+        if (OnLandEvent == null)
+            OnLandEvent = new UnityEvent();
+
+        if (OnCrouchEvent == null)
+            OnCrouchEvent = new BoolEvent();
     }
 
-    private void Start()
+    private void FixedUpdate()
     {
-        spriteRenderer = GetComponent<SpriteRenderer>();
-        rigidbody2D = GetComponent<Rigidbody2D>();
+        bool wasGrounded = m_Grounded;
+        m_Grounded = false;
+
+        // The player is grounded if a circlecast to the groundcheck position hits anything designated as ground
+        // This can be done using layers instead but Sample Assets will not overwrite your project settings.
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(m_GroundCheck.position, k_GroundedRadius, m_WhatIsGround);
+        for (int i = 0; i < colliders.Length; i++)
+        {
+            if (colliders[i].gameObject != gameObject)
+            {
+                m_Grounded = true;
+                if (!wasGrounded)
+                    OnLandEvent.Invoke();
+            }
+        }
     }
 
 
-    private void Update()
+    public void Move(float moveX, float moveY, bool crouch, bool jump)
     {
-        HandleMovement();
 
-    }
+        Vector3 targetVelocity = new Vector2(moveX, moveY);
+        transform.position += targetVelocity * walkSpeed;
 
-    private void HandleMovement()
-    {
-        // Raycast down und nach spriterendere ausrichtung und zu shcauen ob auf boden und ob eine
-        // wand vor einem ist
-        // wenn boden false und wand true dann mit Taste Strg festhalten
-
-
-        // W
-        // Jump
-
-        // S
-        // Crouch
-
-        // A
-        // minus X
-
-
-        // D
-        // plus X
-
-        // Space
-        // Dash
-
-        // Strg
-        // Festhalten
-
-        // E
-        // Interact
-
-        // F 
-        // Punch
+        // If the input is moving the player right and the player is facing left...
+        if (moveX > 0 && !m_FacingRight)
+        {
+            Flip();
+        }
+        // Otherwise if the input is moving the player left and the player is facing right...
+        else if (moveX < 0 && m_FacingRight)
+        {
+            Flip();
+        }
 
     }
 
     private void Flip()
     {
-        spriteRenderer.flipX = true;
+        // Switch the way the player is labelled as facing.
+        m_FacingRight = !m_FacingRight;
+
+        // Multiply the player's x local scale by -1.
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
     }
 
-
-   
 }
---
+++ Assets/_Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace Assets._Scripts.Player
+{
+    class PlayerMovement : MonoBehaviour
+    {
+
+        [SerializeField]
+        private LayerMask plattformslayerMask;
+        private Rigidbody2D rigidbody2D;
+        private SpriteRenderer spriteRenderer;
+        private BoxCollider2D boxCollider2D;
+        private Animator animator;
+        private PlayerController controller;
+
+
+
+        public float speed;
+        public float jumpVelocity;
+
+        private bool jump = false;
+        private bool crouch = false;
+        private bool isgrounded = false;
+        private bool isFacingRight = false;
+
+        [Header("Events")]
+        [Space]
+
+        public UnityEvent OnLandEvent;
+
+
+        private void Awake()
+        {
+            if (OnLandEvent == null)
+            {
+                OnLandEvent = new UnityEvent();
+            }
+
+        }
+
+        private void Start()
+        {
+            spriteRenderer = GetComponent<SpriteRenderer>();
+            rigidbody2D = GetComponent<Rigidbody2D>();
+            boxCollider2D = GetComponent<BoxCollider2D>();
+            animator = GetComponent<Animator>();
+            controller = GetComponent<PlayerController>();
+        }
+
+
+        private void Update()
+        {
+            HandleMovement();
+            
+        }
+
+        private void FixedUpdate()
+        {
+            
+            if (IsGrounded() == true)
+            {
+                OnLandEvent.Invoke();
+            }
+        }
+
+        private void HandleMovement()
+        {
+            float moveY = 0f;
+            float horizontalMove = 0f;
+            // Raycast down und nach spriterendere ausrichtung und zu shcauen ob auf boden und ob eine
+            // wand vor einem ist
+            // wenn boden false und wand true dann mit Taste Strg festhalten
+
+
+            // W
+            // Jump
+            if (IsGrounded() && Input.GetKey(KeyCode.W))
+            {
+                rigidbody2D.velocity = Vector2.up * jumpVelocity;
+                animator.SetBool("jump", true);
+            }
+
+            // S
+            // Crouch
+            if (Input.GetKeyDown(KeyCode.S))
+            {
+                boxCollider2D.enabled = crouch;
+                crouch = !crouch;
+            }
+
+            // Move with A and D
+            horizontalMove = Input.GetAxisRaw("Horizontal");
+            animator.SetFloat("speed", Mathf.Abs(horizontalMove));
+            if (horizontalMove > 0 && isFacingRight)
+            {
+                Flip();
+            }
+            // Otherwise if the input is moving the player left and the player is facing right...
+            else if (horizontalMove < 0 && !isFacingRight)
+            {
+                Flip();
+            }
+
+
+            // Space
+            // Dash
+            if (Input.GetKey(KeyCode.Space))
+            {
+
+            }
+
+            // Strg
+            // Festhalten
+
+            // E
+            // Interact
+            if (Input.GetKey(KeyCode.E))
+            {
+
+            }
+
+            // F 
+            // Punch
+            if (Input.GetKey(KeyCode.F))
+            {
+                animator.SetBool("punch", true);
+            }
+
+            Vector3 moveDir = new Vector3(horizontalMove, moveY);
+            transform.position += moveDir * speed * Time.deltaTime;
+
+        }
+
+        private bool IsGrounded()
+        {
+            var rayCast = Physics2D.BoxCast(boxCollider2D.bounds.center, boxCollider2D.bounds.size, 0f, Vector2.down, .1f, plattformslayerMask);
+            return rayCast.collider != null;
+        }
+        private void Flip()
+        {
+            isFacingRight = !isFacingRight;
+            //Vector3 theScale = transform.localScale;
+            //theScale.x *= -1;
+            //transform.localScale = theScale;
+            this.spriteRenderer.flipX = isFacingRight;
+        }
+
+        public void OnLanding()
+        {
+            animator.SetBool("jump", false);
+        }
+
+
+    }
+}
