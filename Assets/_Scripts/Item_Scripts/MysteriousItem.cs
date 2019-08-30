9d41750ea472d12bf5f109f9da0e273c5a1e6266
Luciano Jung <you@example.com>
2019-08-30 18:36:41 +0200

    Blinzel Manager works

---
+++ Assets/_Scripts/Item_Scripts/MysteriousItem.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New MysteriousItem", menuName = "Inventory/MysteriousItem")]
+public class MysteriousItem : ScriptableObject
+{
+    public List<Item> items = new List<Item>();
+    public Item activeItem;
+    public int itemPos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        itemPos = 0;
+
+        //activeItem = items.Get(itemPos);
+    }
+
+    public bool Add(Item item)
+    {
+        items.Add(item);
+        return true;
+    }
+
+    public void Remove(Item item)
+    {
+        items.Remove(item);
+    }
+
+    public List<Item> getItems()
+    {
+        return this.items;
+    }
+}
