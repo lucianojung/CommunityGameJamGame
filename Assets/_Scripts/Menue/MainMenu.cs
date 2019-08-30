0d8e3e315991a96b7aea8375f191a759d33ee7b3
Maximilian Stümpfl <maximilian2304@t-online.de>
2019-08-30 12:23:01 +0200

    Added simple mainmenu

---
+++ Assets/_Scripts/Menue/MainMenu.cs
@@ -0,0 +1,23 @@
+﻿
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Assets._Scripts.Menue
+{
+    class MainMenu : MonoBehaviour
+    {
+
+
+        public void PlayGame()
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex +1);
+        }
+
+        public void QuitGame()
+        {
+            Application.Quit();
+        }
+
+
+    }
+}
